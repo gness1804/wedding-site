05d3b306666938290ae13bfc23602f7b779fb9cb
gness1804 <graham.nessler@gmail.com>
2019-04-07 16:05:40 -0500

    Add stylelint

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <p>I am the main component.</p>
+        <p>I am the main component.</p>
       </div>
     );
   }
