02f849a178940ea9d9383c8392cbb20e46edcc2e
gness1804 <graham.nessler@gmail.com>
2019-04-07 15:45:32 -0500

    Fix eslint errors

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable-next-line no-unused-vars */
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,9 @@
+/* global it */
+
+/* eslint-disable-next-line no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
+/* eslint-disable-next-line no-unused-vars */
 import App from './App';
 
 it('renders without crashing', () => {
