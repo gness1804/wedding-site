e0025fa5df66d334d64aaeeb147681dcd156e3c4
gness1804 <graham.nessler@gmail.com>
2019-05-02 20:58:41 -0500

    Add confirmation toggle if user is coming

--- client/src/components/RSVP.js
+++ client/src/components/RSVP.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import axios from 'axios';
 import SiteContext from '../context';
 import mdl from '../design/masterDesignLanguage';
 import '../styles/RSVP.css';
 
 const RSVP = () => {
+  const [isComing, setIsComing] = useState(true);
   const { state } = useContext(SiteContext);
   const {
     pageContent: { rsvp },
@@ -20,6 +21,7 @@ const RSVP = () => {
   }
 
   const sendData = async () => {
+    // TODO: be sure to add try catch block
     await axios.post(
       'http://localhost:8004/server/rsvpService.js', // replace with actual endpoint
       {
@@ -33,7 +35,11 @@ const RSVP = () => {
     );
   };
 
-  const { title } = rsvp;
+  const { title, isComingYes, isComingNo } = rsvp;
+  let showBottomPartClass = 'hide';
+  if (isComing) {
+    showBottomPartClass = 'show';
+  }
 
   return (
     <div
@@ -46,6 +52,39 @@ const RSVP = () => {
       >
         {title}
       </h2>
+      {/*access* code entry goes here */}
+      {/*name forms go here*/}
+      <form action="#" className="confirmation-form">
+        <p>
+          <label>
+            <input
+              name="confirmation-radio"
+              type="radio"
+              checked={isComing}
+              onChange={() => {
+                setIsComing(true);
+              }}
+            />
+            <span className="radio-button-text">{isComingYes}</span>
+          </label>
+        </p>
+        <p>
+          <label>
+            <input
+              name="confirmation-radio"
+              type="radio"
+              checked={!isComing}
+              onChange={() => {
+                setIsComing(false);
+              }}
+            />
+            <span className="radio-button-text">{isComingNo}</span>
+          </label>
+        </p>
+      </form>
+      <div className={`is-coming-form-container ${showBottomPartClass}`}>
+        <p>I should only show when the user is coming.</p>
+      </div>
       <button onClick={sendData}>Send</button>
     </div>
   );
