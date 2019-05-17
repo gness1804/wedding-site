cbcf664f95514b3f5bce27c12a771d9744bbb5a8
gness1804 <graham.nessler@gmail.com>
2019-05-16 19:30:18 -0500

    Add newline to preCommit

--- scripts/preCommit.js
+++ scripts/preCommit.js
@@ -96,7 +96,7 @@ getCurrentBranch()
   .then(() => promisifiedExec('npm run stylelint'))
   .then(() => promisifiedExec('npm run test'))
   .then(() => {
-    process.stdout.write('Sucessfully passed pre-commit checks.');
+    process.stdout.write('Sucessfully passed pre-commit checks. \n');
     process.exit(0);
   })
   .catch(err => {
