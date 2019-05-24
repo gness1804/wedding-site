a955e9f6c81eec948ad9dfe25c08f39bc7cc36c1
gness1804 <graham.nessler@gmail.com>
2019-05-23 21:41:56 -0500

    Style google maps embed iframe

--- src/components/Reception.js
+++ src/components/Reception.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import mdl from '../design/masterDesignLanguage';
+import '../styles/Reception.css';
 
 const Reception = () => {
   const mapKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
   return (
-    <div className={`card page-component z-depth-1 ${mdl.colors.primary}`}>
+    <div className={`card page-component z-depth-1 center ${mdl.colors.primary}`}>
       <p>I am the reception component.</p>
       {mapKey && (
         <iframe
-          // width="600"
-          // height="450"
+          className="google-map-container"
           frameBorder={0}
-          // style="border:0"
           src={`https://www.google.com/maps/embed/v1/place?key=${mapKey}
         &q=Steiner+Ranch+Steakhouse+Austin+TX`}
           allowFullScreen
