2565bcc82273e233d488fdb2e04373aa19fed522
gness1804 <graham.nessler@gmail.com>
2019-06-06 20:32:55 -0500

    Add JSDoc for calcCountdownDate

--- src/helpers/calcCountdownDate.js
+++ src/helpers/calcCountdownDate.js
@@ -1,3 +1,10 @@
+/**
+ * Calculates the number of days between the target date and today
+ * @param {string} targetDate - the date in the future we are counting down from.
+ * @param {string } earlierDate - today's date
+ * @returns {number} - the number of days between the two dates
+ */
+
 const calcCountdownDate = (targetDate, earlierDate) => {
   const uglifiedTargetDate = Date.parse(targetDate);
   let uglifiedEarlierDate = Date.now();
