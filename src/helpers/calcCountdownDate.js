a6165fcc6564c0693392ad6ca8575ade97b14142
gness1804 <graham.nessler@gmail.com>
2021-07-11 14:46:35 -0500

    make the countdown function return undefined if target date is earlier than today

--- src/helpers/calcCountdownDate.js
+++ src/helpers/calcCountdownDate.js
@@ -1,8 +1,8 @@
 /**
  * Calculates the number of days between the target date and today
  * @param {string} targetDate - the date in the future we are counting down from.
- * @param {string } earlierDate - today's date
- * @returns {number} - the number of days between the two dates
+ * @param {string } earlierDate - today's date. Defaults to Date.now() if no value is passed.
+ * @returns {number} - the number of days between the two dates. If targetDate is later than earlier date, this will display number of days remaining; else, the function returns undefined.
  */
 
 const calcCountdownDate = (targetDate, earlierDate) => {
@@ -12,6 +12,7 @@ const calcCountdownDate = (targetDate, earlierDate) => {
     uglifiedEarlierDate = Date.parse(earlierDate);
   }
   const diff = uglifiedTargetDate - uglifiedEarlierDate;
+  if (diff <= 0) return undefined;
   return diff / (1000 * 60 * 60 * 24);
 };
 
